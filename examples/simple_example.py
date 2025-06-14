3e292ac515979fd38622e20b42e876fe4d814058
Alessandro Pischedda <alessandro.pischedda@gmail.com>
2025-06-14 13:03:57 +0200

    update with new examples

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -8,9 +8,10 @@ def greet(name: str = "World"):
     print(f"Hello {name}!")
 
 
-@cli.command(name="sum", help_short="Add two numbers", help_long="Add two numbers and print the result.")
+@cli.command(name="sum", menu="Sum two numbers", usage="Add two numbers and print the result.")
 def add(a: int, b: int):
     print(f"The result is: {a + b}")
+
 
 if __name__ == "__main__":
     cli.run()
--- examples/subcmd_examples.py
+++ examples/subcmd_examples.py
@@ -7,29 +7,34 @@ cli = CLI()
 # add the commands to the root menu
 @cli.command()
 def greet(name: str = "World"):
-    """Greet a user by name."""
+    """<menu>Greet a user by name.</menu>"""
     print(f"Hello {name}!")
 
+# create a sub menu called 'math'
 math_submenu = MenuGroup("math", "Some math operations", "Some math operations")
+
 @math_submenu.command()
 def add(a: int, b: int):
-    """Add two numbers."""
+    """<menu>Add two numbers.</menu>"""
     print(f"The result is: {a + b}")
 
 @math_submenu.command()
 def add_list(numbers: List[int]):
-    """Add N numbers."""
+    """<menu>Add N numbers.</menu>
+       <usage>
+       Makes the sum of N interger.
+       </usage>
+    """
     tot = sum(numbers)
     print(f"The result is: {tot}")
 
 @math_submenu.command()
 def sub(a: int, b: int):
-    """Subtract two numbers."""
+    """<menu>Subtract two numbers.</menu>"""
     print(f"The result is: {a - b}")
 
 # add math submenu to the root menu
 cli.add_group(math_submenu)
-
 
 if __name__ == "__main__":
     cli.run()
